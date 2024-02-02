a1fd638c48cfc8a4598c205c9ac446992c9a742b
Juanan Pereira <juanan.pereira@ehu.eus>
2024-02-02 16:30:21 +0100

    refactor ingest into a function

--- api/routes/test.py
+++ api/routes/test.py
@@ -95,67 +95,73 @@ def convert_time_to_seconds(time_str):
     return total_seconds
 
 
+def ingest():
+    # RESET THE DATABASE collection
+    chatbot.db.reset()
+
+    date = datetime.today().strftime('%Y-%m-%d')
+
+    # URL of the directory listing
+    directory_url = 'http://192.168.64.1:9000/'
+
+    # Make an HTTP GET request to retrieve the directory listing
+    response = requests.get(directory_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Use BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find all links in the directory listing
+        links = soup.find_all('a')
+
+        # Filter out links to JSON files
+        json_file_links = [link.get('href') for link in links if link.get('href').endswith('.json')]
+
+        # Iterate over each JSON file link
+        for json_link in json_file_links:
+            # Construct the full URL for the JSON file
+            json_url = directory_url + json_link
+            # Make an HTTP GET request to download the JSON file
+            json_response = requests.get(json_url)
+
+            # Make sure the request was successful
+            if json_response.status_code == 200:
+                # Parse the JSON content
+                data = json_response.json()
+
+                for item in data:
+
+                    # Extract information and create metadata
+
+                    start_time = item["timestamp_start"]
+                    start_seconds = convert_time_to_seconds(start_time)
+                    item["url"] = f'{item["url"]}&t={start_seconds}s'
+
+
+                    metadata = {
+                        "number": item["number"],
+                        "timestamp_start": item["timestamp_start"],
+                        "timestamp_end": item["timestamp_end"],
+                        "author": "Jose",
+                        "date": date,
+                        "kind": item["kind"],
+                        "file_number": item["file_number"],
+                        "filename": item["filename"],
+                        "url": item["url"],
+                    }
+                    
+                    chatbot.add( item["text"],  data_type="text", metadata = metadata)
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+
+
+
 chatbot = App.from_config(config=app_config)
 # chatbot.config.logger.parent.setLevel("DEBUG")
 
-# RESET THE DATABASE collection
-chatbot.db.reset()
-
-date = datetime.today().strftime('%Y-%m-%d')
-
-# URL of the directory listing
-directory_url = 'http://192.168.64.1:9000/'
-
-# Make an HTTP GET request to retrieve the directory listing
-response = requests.get(directory_url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Use BeautifulSoup to parse the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all links in the directory listing
-    links = soup.find_all('a')
-
-    # Filter out links to JSON files
-    json_file_links = [link.get('href') for link in links if link.get('href').endswith('.json')]
-
-    # Iterate over each JSON file link
-    for json_link in json_file_links:
-        # Construct the full URL for the JSON file
-        json_url = directory_url + json_link
-         # Make an HTTP GET request to download the JSON file
-        json_response = requests.get(json_url)
-
-        # Make sure the request was successful
-        if json_response.status_code == 200:
-            # Parse the JSON content
-            data = json_response.json()
-
-            for item in data:
-
-                # Extract information and create metadata
-
-                start_time = item["timestamp_start"]
-                start_seconds = convert_time_to_seconds(start_time)
-                item["url"] = f'{item["url"]}&t={start_seconds}s'
-
-
-                metadata = {
-                    "number": item["number"],
-                    "timestamp_start": item["timestamp_start"],
-                    "timestamp_end": item["timestamp_end"],
-                    "author": "Jose",
-                    "date": date,
-                    "kind": item["kind"],
-                    "file_number": item["file_number"],
-                    "filename": item["filename"],
-                    "url": item["url"],
-                }
-                
-                chatbot.add( item["text"],  data_type="text", metadata = metadata)
-else:
-    print(f"Failed to retrieve data. Status code: {response.status_code}")
+# ingest()
 
 # context = chatbot.search("¿Qué es la inflación?", num_documents=5)
 # print(json.dumps(context))
